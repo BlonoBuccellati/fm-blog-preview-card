b6422dca98d4f1c5a4ebe116aba4196da04094c4
BlonoBuccellati <rk.shimizu.rk@gmail.com>
2025-03-07 23:24:57 +0900

    chore:setup contents

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
 export default function Home() {
-  return <div>init</div>;
+  return (
+    <div className='bg-custom-yellow min-h-screen'>
+      {/* Card */}
+      <div className=''>
+        {/* Blog Image */}
+        <div>blog image</div>
+        {/* Content */}
+        <div>
+          {/* Category */}
+          {/* publishDate */}
+          {/* title */}
+          {/* description */}
+        </div>
+      </div>
+    </div>
+  );
 }
