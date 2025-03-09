880787c52ddae5f2c2679fcd98d7535cf3795b04
BlonoBuccellati <rk.shimizu.rk@gmail.com>
2025-03-09 12:06:06 +0900

    style: update card styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,43 @@
-import Image from 'next/image';
+import Image from "next/image";
 // import {Image} form "next/"
 export default function Home() {
   return (
-    <div className='bg-custom-yellow min-h-screen flex justify-center items-center'>
+    <div className="bg-custom-yellow flex min-h-screen items-center justify-center">
       {/* Card */}
-      <div className='bg-white w-[384px] h-[594px] flex rounded-2xl border-2 drop-shadow-[10px_10px_0px_rgba(0,0,0,1)]'>
+      <div className="flex h-[594px] w-[384px] flex-col gap-6 rounded-2xl border-2 bg-white p-6 drop-shadow-[10px_10px_0px_rgba(0,0,0,1)]">
         {/* Blog Image */}
         <Image
-          src='/assets/images/illustration-article.svg'
-          alt='Blog illustration'
+          src="/assets/images/illustration-article.svg"
+          alt="Blog illustration"
           width={500}
           height={300}
+          className="rounded-2xl"
         />
         {/* Content */}
-        <div>
+        <div className="flex flex-col gap-3">
           {/* Category */}
+          <p className="bg-custom-yellow text-custom-gray-950 prose-sm prose prose-custom-mid h-[29px] w-[82px] rounded border px-3 py-1 text-center font-bold">
+            Learning
+          </p>
           {/* publishDate */}
+          <p>Published 21 Dec 2023</p>
           {/* title */}
+          <h1>HTML & CSS foundation</h1>
           {/* description */}
+          <p>
+            These languages are the backbone of every website, defining
+            structure, content, and presentation
+          </p>
+        </div>
+        {/* Author */}
+        <div>
+          <Image
+            src="/assets/images/image-avatar.webp"
+            alt="Author"
+            width={30}
+            height={30}
+          ></Image>
+          <p>Greg Hooper</p>
         </div>
       </div>
     </div>
