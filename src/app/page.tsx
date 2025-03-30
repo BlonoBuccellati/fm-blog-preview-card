d9c3079c9ca3ca20dc20a0a105ec8f8421af98c9
BlonoBuccellati <rk.shimizu.rk@gmail.com>
2025-03-30 17:30:01 +0900

    fix: mainタグの追加（アクセシビリティのため）
    アクセシリビリティのため、mainタグ(ランドマーク）の追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import BlogPreviewCard from "./_components/blog-preview-card";
 
 export default function Home() {
   return (
-    <div className="bg-custom-yellow flex min-h-screen items-center justify-center">
-      <BlogPreviewCard />
-    </div>
+    <main>
+      <div className="bg-custom-yellow flex min-h-screen items-center justify-center">
+        <BlogPreviewCard />
+      </div>
+    </main>
   );
 }
